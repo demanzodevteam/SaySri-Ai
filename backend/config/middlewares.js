215f8dc6271af4385a01a86f072b16025faa43f7
vibindem <vibin.sekhar@demanzo.com>
2025-11-27 11:02:34 +0530

    new backend created

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,13 +1,4 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000'], // allow your frontend
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    },
-  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,30 +1,57 @@
-import Image from "next/image";
 import EnterpriseAI from "../components/Home/Enterprise";
 import MeetSaysri from "../components/Home/MeetSaysri";
-import FirstStep from "../components/Home/FirstStep"
-import About from "../components/Home/About"
-import LatestInAI from "../components/Home/LatestinAI"
-import TrustSlider from "../components/Home/TrustSlider"
+import FirstStep from "../components/Home/FirstStep";
+import About from "../components/Home/About";
+import LatestInAI from "../components/Home/LatestinAI";
+import TrustSlider from "../components/Home/TrustSlider";
 import VideoSlider from "../components/Home/VideoSlider";
-import WhyHiring from "../components/WhyHiring";
-import Book from "../components/Book"
-import ExperienceAI from "../components/ExperienceAI"
-import Testimonial from "../components/Testimonials"
 
-
-export default function Home() {
+export default async function Home() {
+  const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
+  const res = await fetch(`${baseUrl}/api/home?populate[FirstSection][populate]=*`,
+    { next: { revalidate: 60 } } // optional caching
+  );
+  const json = await res.json();
+  
+  const cms = json?.data?.attributes || {};
+  console.log(cms)
   return (
     <main className="flex w-full flex-col items-center justify-between ">
-        <EnterpriseAI />
-        <MeetSaysri />
-        <TrustSlider />
-        <FirstStep />
-        <VideoSlider />
-        {/* <Book /> */}
-        {/* <Testimonial /> */}
-        <About />
-        {/* <ExperienceAI /> */}
-        <LatestInAI />
+      <EnterpriseAI cms={cms.FirstSection} />
+      <MeetSaysri cms={cms.meetSaysri} />
+      <TrustSlider cms={cms.trustSlider} />
+      <FirstStep cms={cms.firstStep} />
+      <VideoSlider cms={cms.videoSlider} />
+      <About cms={cms.about} />
+      <LatestInAI cms={cms.latestInAI} />
     </main>
   );
 }
+
+
+
+
+// import Image from "next/image";
+// import EnterpriseAI from "../components/Home/Enterprise";
+// import MeetSaysri from "../components/Home/MeetSaysri";
+// import FirstStep from "../components/Home/FirstStep"
+// import About from "../components/Home/About"
+// import LatestInAI from "../components/Home/LatestinAI"
+// import TrustSlider from "../components/Home/TrustSlider"
+// import VideoSlider from "../components/Home/VideoSlider";
+// import { strapiURL} from "../../lib/strapi";
+
+
+// export default function Home() {
+//   return (
+//     <main className="flex w-full flex-col items-center justify-between ">
+//         <EnterpriseAI />
+//         <MeetSaysri />
+//         <TrustSlider />
+//         <FirstStep />
+//         <VideoSlider />
+//         <About />
+//         <LatestInAI />
+//     </main>
+//   );
+// }
